16f920ae376d1b4f7c63ed7a8c83b0a9c7617c0d
Giovanni Rosales <grosales@cicese.mx>
2021-02-12 19:52:56 -0700

    Uncontrolled inputs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react';
 
-const FancyInput = React.forwardRef((props, ref) => (
-  <div>
-    <input type="text" ref={ref}/>
-  </div>
-));
+const Unicorn = () => (
+  <span role='img' aria-label='unicornio'>
+    🦄 holis
+  </span>
+)
 
+class InputNoControlado extends Component {
+  nombre = React.createRef()
+  email = React.createRef()
 
-class App extends Component {
-  entrada = React.createRef()
+  handleClick = () => {
+    const nombre = this.nombre.current.value;
+    const email = this.email.current.value;
 
-  componentDidMount() {
-    console.log(this.entrada);
+    this.props.onSend({nombre, email});
   }
 
   render() {
     return (
       <div>
-        <h1>Reenvío de refs</h1>
-        <FancyInput ref={this.entrada} />
+        <input 
+        type="text"
+        ref={this.nombre}
+        placeholder="Nombre" />
+        <input 
+        type="text"
+        ref={this.email}
+        placeholder="Email" />
+        <input type="submit" onClick={this.handleClick} />
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+
+  send = (data) => {
+    console.log(`${data.nombre}, ${data.email}`)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Inputs no controlados Refs <Unicorn /></h1>
+        <InputNoControlado 
+          onSend={this.send}
+        />
       </div>
     )
   }
